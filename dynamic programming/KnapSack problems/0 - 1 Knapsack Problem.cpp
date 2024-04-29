fe1131572ea6e6fcd837c0e971adb18c208bec1f
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-04-29 22:18:04 +0530

    Update 0 - 1 Knapsack Problem.cpp

--- dynamic programming/KnapSack problems/0 - 1 Knapsack Problem.cpp
+++ dynamic programming/KnapSack problems/0 - 1 Knapsack Problem.cpp
@@ -28,6 +28,34 @@
         return dp[n][W];
     }
 }
+//top down
+ int helper(int W, int wt[], int val[], int n, vector<vector<int>>& dp) {
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<W+1;j++){
+            if(i==0 || j==0){
+                dp[i][j]==0;
+            }
+        }
+    }
+    
+   for(int i=1;i<n+1;i++){
+       for(int j=1;j<W+1;j++){
+           if(j >= wt[i - 1]) {
+        dp[i][j] = max(val[i - 1] +dp[i-1][j-wt[i-1]],dp[i-1][j]);
+    }
+    else {
+        dp[i][j] =dp[i-1][j];
+       
+    }
+       }
+   }
+    
+    return dp[n][W];
+}
+int knapSack(int W, int wt[], int val[], int n) {
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1)); 
+    return helper(W, wt, val, n, dp);
+}
 
 int knapSack(int W, int wt[], int val[], int n) {
     vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1)); // Corrected initialization
