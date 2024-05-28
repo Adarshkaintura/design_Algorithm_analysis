077ac0c76b5e580454d18738be9a12a140181c3e
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-05-28 18:17:07 +0530

    Update countOfSubset of a given sum.cpp

--- dynamic programming/KnapSack problems/countOfSubset of a given sum.cpp
+++ dynamic programming/KnapSack problems/countOfSubset of a given sum.cpp
@@ -34,3 +34,43 @@ int main() {
     cout << "The total count is: " << dp[arr.size()][6];
     return 0;
 }
+
+//optimized
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int countSubsetsWithSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+    
+    // Initialize the dp array
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = 1; // There is one way to make a sum of 0: use the empty subset
+    }
+    
+    // Fill the dp array
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= target; j++) {
+            if (nums[i - 1] <= j) {
+                dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+            } else {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+    
+    return dp[n][target];
+}
+
+int main() {
+    vector<int> nums = {2, 3, 5, 6, 8, 10};
+    int target = 10;
+    
+    int result = countSubsetsWithSum(nums, target);
+    cout << "Number of subsets with sum " << target << " is: " << result << endl;
+
+    return 0;
+}
+
