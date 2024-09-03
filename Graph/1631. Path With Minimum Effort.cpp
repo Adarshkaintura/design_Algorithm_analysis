dd9ed21d764d7dd0143176e3b3f2b859f5fc6e8c
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2024-09-03 22:00:02 +0530

    Create 1631. Path With Minimum Effort.cpp

---
+++ Graph/1631. Path With Minimum Effort.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+vector<vector<int>> dirs = {
+                {-1,0},
+        {0,-1},         {0,1},
+                {1, 0}
+    };
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights[0].size();
+        auto isSafe =
+            [&](int x, int y) {
+                return x>=0 && x<m && y>=0 &&y<n;
+            };
+         priority_queue<pair<int, pair<int, int>>,
+                             vector<pair<int, pair<int, int>>>,
+                             greater<pair<int, pair<int, int>>>>
+                pq;
+        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
+        result[0][0] = 0;
+        pq.push({0, {0, 0}});
+
+        while (!pq.empty()) {
+            int diff = pq.top().first;
+            auto cord = pq.top().second;
+            int x = cord.first;
+            int y = cord.second;
+            pq.pop();
+            if (x == m - 1 && y == n - 1) {
+                return diff;
+            }
+            for(auto dir:dirs){
+                int x_=x+dir[0];
+                int y_=y+dir[1];
+                if(isSafe(x_,y_)){
+                    int absdiff=abs(heights[x][y]-heights[x_][y_]);
+                    int ansdiff=max(absdiff,diff);
+                    if(result[x_][y_]>ansdiff){
+                        result[x_][y_]=ansdiff;
+                        pq.push({ansdiff,{x_,y_}});
+                    }
+                }
+            }
+        }
+         return result[m-1][n-1];
+    }
+};
