d0d868a966e8ed957253de5e36d022c9cc2929ef
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2025-04-16 00:03:22 +0530

    Update 287. Find the Duplicate Number.cpp

--- Array/287. Find the Duplicate Number.cpp
+++ Array/287. Find the Duplicate Number.cpp
@@ -15,3 +15,16 @@ public:
        return t;
     }
 };
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& arr) {
+      sort(arr.begin(),arr.end());
+      for(int i=0;i<arr.size()-1;i++){
+        if(arr[i]==arr[i+1]){
+            return arr[i];
+        }
+      }
+      return 0;
+    }
+};
