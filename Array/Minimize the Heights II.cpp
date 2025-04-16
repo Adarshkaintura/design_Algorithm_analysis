92403ef87898aaa138a98e2d73cd19540777d0dd
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2025-04-16 23:36:49 +0530

    Update Minimize the Heights II.cpp

--- Array/Minimize the Heights II.cpp
+++ Array/Minimize the Heights II.cpp
@@ -31,3 +31,19 @@
       return mini;
     }
 ///optimzied
+
+ int getMinDiff(vector<int> &arr, int k) {
+      int n=arr.size();
+      sort(arr.begin(),arr.end());
+      int ansi=arr[n-1]-arr[0];
+      int largest=arr[n-1]-k;
+      int smallest=arr[0]+k;
+      int mn,mx;
+      for(int i=0;i<n-1;i++){
+          mn=min(smallest,arr[i+1]-k);
+          mx=max(largest,arr[i]+k);
+          if(mn<0) continue;
+          ansi=min(ansi,mx-mn);
+      }
+      return ansi;
+    }
