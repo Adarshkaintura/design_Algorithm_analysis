99dde581090f44a105ab64a8ca213155238a0f1d
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2025-06-28 21:40:36 +0530

    Create 881. Boats to Save People.cpp

---
+++ Greedy Algorithm/881. Boats to Save People.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int boat=0;
+        sort(people.begin(),people.end());
+        int l=0,r=people.size()-1;
+        while(l<=r){
+            if(people[l]+people[r]<=limit){
+                boat++;
+                l++;
+                r--;
+            }else{
+              r--;
+              boat++;
+            }
+        }
+        return boat;
+    }
+};
