61cbccedd1d13d472e36c5df9540cb576abbb197
Adarsh Kaintura <146357978+Adarshkaintura@users.noreply.github.com>
2025-07-31 01:53:01 +0530

    Create meetingRooms.cpp

---
+++ PenguinTask/JMAN Group/meetingRooms.cpp
@@ -0,0 +1,73 @@
+//question- /* Problem Description
+Given an 2D integer array A of size N x 2 denoting time intervals of different meetings.
+
+ 
+
+Where:
+
+ 
+
+    A[i][0] = start time of the ith meeting.
+    A[i][1] = end time of the ith meeting.
+
+ 
+
+Find the minimum number of conference rooms required so that all meetings can be done.
+
+Note :- If a meeting ends at time t, another meeting starting at time t can use the same conference room
+
+ 
+
+
+Problem Constraints
+
+1 <= N <= 105
+
+ 
+
+0 <= A[i][0] < A[i][1] <= 2 * 109
+
+ 
+
+
+Input Format
+
+The only argument given is the matrix A.
+
+
+Output Format
+
+Return the minimum number of conference rooms required so that all meetings can be done.
+
+
+Example Input
+
+Input 1:
+
+ A = [      [0, 30]        0-------------------------------------30
+            [5, 10]           5-------10
+            [15, 20]                         15--------20
+     ]
+
+Input 2:
+
+ A =  [     [1, 18]      1-------------------------------18
+            [18, 23]                                     18---------23
+            [15, 29]                            15-------------------------------29
+            [4, 15]            4----------------15
+            [2, 11]        2----------------11
+            [5, 13]              5-------------13
+      ]
+
+
+
+Example Output
+
+Output 1:
+
+ 2
+
+Output 2:
+
+ 4
+*/
